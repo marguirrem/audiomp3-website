3ad74411cafcf4ffa105f732efe7a6c22b2d5193
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2018-01-11 22:40:38 -0600

    link songs,albums and artists added to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,18 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+
+                            <li>
+                                <a href="#">Artistas</a>
+                            </li>
+                            
+                            <li>
+                                <a href="#">Canciones</a>
+                            </li>
+                            
+                            <li>
+                                <a href="#">Albums</a>
+                            </li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->username }} <span class="caret"></span>
