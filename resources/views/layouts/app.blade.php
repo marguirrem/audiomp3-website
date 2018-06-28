fad651b3851484d06041f9b2eecfdfaa752a5d39
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2018-06-27 21:39:48 -0600

    fontawesome and icon for login added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
 
   <title>{{ config('app.name', 'Laravel') }}</title>
 
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
   <!-- Styles -->
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
